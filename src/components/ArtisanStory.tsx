f8ce27a4c16b05d215a1b759954124168093c3cd
raviiiiiiiiiiiiiiiiiiiiiiiiiii <ravi2009u@gmail.com>
2026-04-15 09:49:49 +0530

    Update ArtisanStory.tsx

--- src/components/ArtisanStory.tsx
+++ src/components/ArtisanStory.tsx
@@ -1,5 +1,5 @@
 import { motion } from "motion/react";
-import { Button } from "@/components/ui/Button";
+import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
 
 export default function ArtisanStory() {
